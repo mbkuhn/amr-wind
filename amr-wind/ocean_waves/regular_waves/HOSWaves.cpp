a8e9a9ab0cad51beeb4076032af4dbcffdc47e21
Michael Kuhn <mkuhn@nrel.gov>
2022-07-13 17:02:55 -0600

    starting point for HOS waves. Initialization works for a limited case.

---
+++ amr-wind/ocean_waves/regular_waves/HOSWaves.cpp
@@ -0,0 +1,11 @@
+#include "amr-wind/ocean_waves/regular_waves/HOSWaves.H"
+#include "amr-wind/ocean_waves/regular_waves/hos_waves_ops.H"
+#include "amr-wind/ocean_waves/OceanWavesModel.H"
+
+namespace amr_wind {
+namespace ocean_waves {
+
+template class OWModel<HOSWaves>;
+
+} // namespace ocean_waves
+} // namespace amr_wind
