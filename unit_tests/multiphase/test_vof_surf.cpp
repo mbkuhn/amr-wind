b8819fa2c7b33acc296163e7b80bab5266ab6f05
Michael Kuhn <mkuhn@nrel.gov>
2023-01-05 12:56:35 -0700

    adding a range of tests with more complex planes

--- unit_tests/multiphase/test_vof_surf.cpp
+++ unit_tests/multiphase/test_vof_surf.cpp
@@ -241,12 +241,12 @@ TEST_F(VOFSurfTest, simple_half)
     // In x
     {
         // Input quantities: normal vector
-        amrex::Real normx = 1.0;
-        amrex::Real normy = 0.0;
-        amrex::Real normz = 0.0;
+        const amrex::Real normx = 1.0;
+        const amrex::Real normy = 0.0;
+        const amrex::Real normz = 0.0;
 
         // Get alpha based on inputs
-        amrex::Real alpha =
+        const amrex::Real alpha =
             amr_wind::multiphase::volume_intercept(normx, normy, normz, VOF);
 
         // Perform calculations
@@ -264,12 +264,12 @@ TEST_F(VOFSurfTest, simple_half)
     // In y
     {
         // Input quantities: normal vector
-        amrex::Real normx = 0.0;
-        amrex::Real normy = 1.0;
-        amrex::Real normz = 0.0;
+        const amrex::Real normx = 0.0;
+        const amrex::Real normy = 1.0;
+        const amrex::Real normz = 0.0;
 
         // Get alpha based on inputs
-        amrex::Real alpha =
+        const amrex::Real alpha =
             amr_wind::multiphase::volume_intercept(normx, normy, normz, VOF);
 
         // Perform calculations
@@ -287,12 +287,12 @@ TEST_F(VOFSurfTest, simple_half)
     // In z
     {
         // Input quantities: normal vector
-        amrex::Real normx = 0.0;
-        amrex::Real normy = 0.0;
-        amrex::Real normz = 1.0;
+        const amrex::Real normx = 0.0;
+        const amrex::Real normy = 0.0;
+        const amrex::Real normz = 1.0;
 
         // Get alpha based on inputs
-        amrex::Real alpha =
+        const amrex::Real alpha =
             amr_wind::multiphase::volume_intercept(normx, normy, normz, VOF);
 
         // Perform calculations
@@ -319,13 +319,13 @@ TEST_F(VOFSurfTest, other_half)
     const amrex::Real zm = 0.0;
     const amrex::Real zp = 0.25;
     // Input quantities: normal vector and vof
-    amrex::Real normx = 0.0;
-    amrex::Real normy = 0.0;
-    amrex::Real normz = 1.0;
-    amrex::Real VOF = 0.5;
+    const amrex::Real normx = 0.0;
+    const amrex::Real normy = 0.0;
+    const amrex::Real normz = 1.0;
+    const amrex::Real VOF = 0.5;
 
     // Get alpha based on inputs
-    amrex::Real alpha =
+    const amrex::Real alpha =
         amr_wind::multiphase::volume_intercept(normx, normy, normz, VOF);
 
     // Output quantities to be reused
@@ -346,6 +346,227 @@ TEST_F(VOFSurfTest, other_half)
     EXPECT_NEAR(zc, 0.5 * (zm + zp), tol);
     // surface area should be xy area
     EXPECT_NEAR(sa, (xp - xm) * (yp - ym), tol);
+}
+
+TEST_F(VOFSurfTest, select)
+{
+    // Structured cell boundaries
+    const amrex::Real xm = 0.1;
+    const amrex::Real xp = 0.5;
+    const amrex::Real ym = -0.2;
+    const amrex::Real yp = 0.1;
+    const amrex::Real zm = 0.0;
+    const amrex::Real zp = 0.25;
+    // Output quantities to be reused
+    amrex::Real xc = 0.0;
+    amrex::Real yc = 0.0;
+    amrex::Real zc = 0.0;
+    amrex::Real sa = 0.0;
+
+    // Horizontal plane, less than half VOF
+    {
+        // Input quantities: normal vector and vof
+        const amrex::Real normx = 0.0;
+        const amrex::Real normy = 0.0;
+        const amrex::Real normz = 1.0;
+        const amrex::Real VOF = 0.25;
+
+        // Get alpha based on inputs
+        const amrex::Real alpha =
+            amr_wind::multiphase::volume_intercept(normx, normy, normz, VOF);
+
+        // Perform calculations
+        amr_wind::multiphase::surfacearea_center(
+            normx, normy, normz, alpha, xm, xp, ym, yp, zm, zp, sa, xc, yc, zc);
+
+        // Check answers
+        constexpr amrex::Real tol = 1e-11;
+        // centroid of should be center of cell in x, y
+        EXPECT_NEAR(xc, 0.5 * (xm + xp), tol);
+        EXPECT_NEAR(yc, 0.5 * (ym + yp), tol);
+        // 1/4 of cell in z
+        EXPECT_NEAR(zc, zm + 0.25 * (zp - zm), tol);
+        // surface area should be xy area
+        EXPECT_NEAR(sa, (xp - xm) * (yp - ym), tol);
+    }
+    // Horizontal plane, more than half VOF
+    {
+        // Input quantities: normal vector and vof
+        const amrex::Real normx = 0.0;
+        const amrex::Real normy = 0.0;
+        const amrex::Real normz = 1.0;
+        const amrex::Real VOF = 0.75;
+
+        // Get alpha based on inputs
+        const amrex::Real alpha =
+            amr_wind::multiphase::volume_intercept(normx, normy, normz, VOF);
+
+        // Perform calculations
+        amr_wind::multiphase::surfacearea_center(
+            normx, normy, normz, alpha, xm, xp, ym, yp, zm, zp, sa, xc, yc, zc);
+
+        // Check answers
+        constexpr amrex::Real tol = 1e-11;
+        // centroid should be center of cell in x, y
+        EXPECT_NEAR(xc, 0.5 * (xm + xp), tol);
+        EXPECT_NEAR(yc, 0.5 * (ym + yp), tol);
+        // 3/4 of cell in z
+        EXPECT_NEAR(zc, zm + 0.75 * (zp - zm), tol);
+        // surface area should be xy area
+        EXPECT_NEAR(sa, (xp - xm) * (yp - ym), tol);
+    }
+    // Diagonal plane in 2D, half cell
+    {
+        // Input quantities: normal vector and vof
+        const amrex::Real normx = (zp - zm);
+        const amrex::Real normy = 0.0;
+        const amrex::Real normz = (xp - xm);
+        const amrex::Real VOF = 0.5;
+
+        // Account for normalization and modify for unit cell
+        const amrex::Real unitnormx = normx * (xp - xm);
+        const amrex::Real unitnormy = normy * (yp - ym);
+        const amrex::Real unitnormz = normz * (zp - zm);
+        const amrex::Real nmag = unitnormx + unitnormy + unitnormz;
+
+        // Get alpha based on inputs
+        amrex::Real alpha = amr_wind::multiphase::volume_intercept(
+            unitnormx / nmag, unitnormy / nmag, unitnormz / nmag, VOF);
+
+        // Perform calculations
+        amr_wind::multiphase::surfacearea_center(
+            normx, normy, normz, alpha, xm, xp, ym, yp, zm, zp, sa, xc, yc, zc);
+
+        // Check answers
+        constexpr amrex::Real tol = 1e-11;
+        // centroid should be center of cell in x, y, z
+        EXPECT_NEAR(xc, 0.5 * (xm + xp), tol);
+        EXPECT_NEAR(yc, 0.5 * (ym + yp), tol);
+        EXPECT_NEAR(zc, 0.5 * (zm + zp), tol);
+        // surface area should be y length times xz hypotenuse
+        EXPECT_NEAR(
+            sa, sqrt(std::pow(xp - xm, 2) + std::pow(zp - zm, 2)) * (yp - ym),
+            tol);
+    }
+    // Diagonal plane in 2D, eighth of cell
+    {
+        // Input quantities: normal vector and vof
+        const amrex::Real normx = (zp - zm);
+        const amrex::Real normy = 0.0;
+        const amrex::Real normz = (xp - xm);
+        const amrex::Real VOF = 0.125;
+
+        // Account for normalization and modify for unit cell
+        const amrex::Real unitnormx = normx * (xp - xm);
+        const amrex::Real unitnormy = normy * (yp - ym);
+        const amrex::Real unitnormz = normz * (zp - zm);
+        const amrex::Real nmag = unitnormx + unitnormy + unitnormz;
+
+        // Get alpha based on inputs
+        amrex::Real alpha = amr_wind::multiphase::volume_intercept(
+            unitnormx / nmag, unitnormy / nmag, unitnormz / nmag, VOF);
+
+        // Perform calculations
+        amr_wind::multiphase::surfacearea_center(
+            normx, normy, normz, alpha, xm, xp, ym, yp, zm, zp, sa, xc, yc, zc);
+
+        // Check answers
+        constexpr amrex::Real tol = 1e-11;
+        // centroid should be center of cell in y
+        EXPECT_NEAR(yc, 0.5 * (ym + yp), tol);
+        // quarter of cell in x, z
+        EXPECT_NEAR(xc, xm + 0.25 * (xp - xm), tol);
+        EXPECT_NEAR(zc, zm + 0.25 * (zp - zm), tol);
+        // surface area should be y length times xz hypotenuse
+        EXPECT_NEAR(
+            sa,
+            sqrt(std::pow(0.5 * (xp - xm), 2) + std::pow(0.5 * (zp - zm), 2)) *
+                (yp - ym),
+            tol);
+    }
+    // Diagonal plane in 3D, half of cell
+    {
+        // Input quantities: normal vector and vof
+        const amrex::Real normx = (yp - ym) * (zp - zm);
+        const amrex::Real normy = (xp - xm) * (zp - zm);
+        const amrex::Real normz = 2.0 * (xp - xm) * (yp - ym);
+        const amrex::Real VOF = 0.5;
+
+        // Account for normalization and modify for unit cell
+        const amrex::Real unitnormx = normx * (xp - xm);
+        const amrex::Real unitnormy = normy * (yp - ym);
+        const amrex::Real unitnormz = normz * (zp - zm);
+        const amrex::Real nmag = unitnormx + unitnormy + unitnormz;
+
+        // Get alpha based on inputs
+        amrex::Real alpha = amr_wind::multiphase::volume_intercept(
+            unitnormx / nmag, unitnormy / nmag, unitnormz / nmag, VOF);
+
+        // Perform calculations
+        amr_wind::multiphase::surfacearea_center(
+            normx, normy, normz, alpha, xm, xp, ym, yp, zm, zp, sa, xc, yc, zc);
+
+        // Check answers
+        constexpr amrex::Real tol = 1e-11;
+        // centroid should be center of cell in x, y, z
+        EXPECT_NEAR(xc, 0.5 * (xm + xp), tol);
+        EXPECT_NEAR(yc, 0.5 * (ym + yp), tol);
+        EXPECT_NEAR(zc, 0.5 * (zm + zp), tol);
+        // analytical surface area
+        const amrex::Real a =
+            sqrt(std::pow((xp - xm), 2) + std::pow(0.5 * (zp - zm), 2));
+        const amrex::Real b =
+            sqrt(std::pow((yp - ym), 2) + std::pow(0.5 * (zp - zm), 2));
+        const amrex::Real c = sqrt(
+            std::pow(xp - xm, 2) + std::pow(yp - ym, 2) + std::pow(zp - zm, 2));
+        const amrex::Real s = 0.5 * (a + b + c);
+        const amrex::Real asa = 2. * sqrt(s * (s - a) * (s - b) * (s - c));
+        // surface area check
+        EXPECT_NEAR(sa, asa, tol);
+    }
+    // Diagonal plane in 3D, wedge of cell
+    {
+        // Input quantities: normal vector and vof
+        const amrex::Real normx = (yp - ym) * (zp - zm);
+        const amrex::Real normy = (xp - xm) * (zp - zm);
+        const amrex::Real normz = 2.0 * (xp - xm) * (yp - ym);
+
+        // Area of liquid tetrahedron / area of cell
+        const amrex::Real VOF =
+            ((0.5 * (zp - zm)) * (0.5 * (xp - xm) * (yp - ym))) / 3. /
+            ((xp - xm) * (yp - ym) * (zp - zm));
+
+        // Account for normalization and modify for unit cell
+        const amrex::Real unitnormx = normx * (xp - xm);
+        const amrex::Real unitnormy = normy * (yp - ym);
+        const amrex::Real unitnormz = normz * (zp - zm);
+        const amrex::Real nmag = unitnormx + unitnormy + unitnormz;
+
+        // Get alpha based on inputs
+        amrex::Real alpha = amr_wind::multiphase::volume_intercept(
+            unitnormx / nmag, unitnormy / nmag, unitnormz / nmag, VOF);
+
+        // Perform calculations
+        amr_wind::multiphase::surfacearea_center(
+            normx, normy, normz, alpha, xm, xp, ym, yp, zm, zp, sa, xc, yc, zc);
+
+        // Check answers
+        constexpr amrex::Real tol = 1e-11;
+        // centroid is average of triangular surface nodes
+        EXPECT_NEAR(xc, (2. * xm + xp) / 3., tol);
+        EXPECT_NEAR(yc, (2. * ym + yp) / 3., tol);
+        EXPECT_NEAR(zc, (2.5 * zm + 0.5 * zp) / 3., tol);
+        // analytical surface area
+        const amrex::Real a =
+            sqrt(std::pow((xp - xm), 2) + std::pow(0.5 * (zp - zm), 2));
+        const amrex::Real b =
+            sqrt(std::pow((yp - ym), 2) + std::pow(0.5 * (zp - zm), 2));
+        const amrex::Real c = sqrt(std::pow(xp - xm, 2) + std::pow(yp - ym, 2));
+        const amrex::Real s = 0.5 * (a + b + c);
+        const amrex::Real asa = sqrt(s * (s - a) * (s - b) * (s - c));
+        // surface area check
+        EXPECT_NEAR(sa, asa, tol);
+    }
 }
 
 } // namespace amr_wind_tests
