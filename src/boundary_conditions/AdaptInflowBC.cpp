0ff8aae9fe47ce9f2b8a56efeb69a6d0dd62f6e0
Michael Kuhn <mkuhn@nlr.gov>
2026-08-31 15:26:19 -0600

    put custom BC back with vof-based Neumann

--- src/boundary_conditions/AdaptInflowBC.cpp
+++ src/boundary_conditions/AdaptInflowBC.cpp
@@ -9,7 +9,8 @@ AdaptInflowBC::AdaptInflowBC(Field& field, amrex::Orientation ori)
 void AdaptInflowBC::operator()(Field& /*field*/, const FieldState /*rho_state*/)
 {
     const auto& repo = m_field.repo();
-    const auto& velocity = repo.get_field("velocity");
+    const auto* volume_frac =
+        repo.field_exists("vof") ? &repo.get_field("vof") : nullptr;
     const int ncomp = m_field.num_comp();
     const int idim = m_ori.coordDir();
     const auto islow = m_ori.isLow();
@@ -18,6 +19,9 @@ void AdaptInflowBC::operator()(Field& /*field*/, const FieldState /*rho_state*/)
     const amrex::IntVect iv_dir = {
         static_cast<int>(idim == 0), static_cast<int>(idim == 1),
         static_cast<int>(idim == 2)};
+
+    const amrex::Real small_val = 1.0e-12_rt;
+    bool use_vof = (volume_frac != nullptr);
 
     for (int lev = 0; lev < nlevels; ++lev) {
         const auto& domain = repo.mesh().Geom(lev).Domain();
@@ -35,20 +39,22 @@ void AdaptInflowBC::operator()(Field& /*field*/, const FieldState /*rho_state*/)
                 {static_cast<int>(idim != 0), static_cast<int>(idim != 1),
                  static_cast<int>(idim != 2)});
             const auto& bc_a = m_field(lev).array(mfi);
-            const auto& vel = velocity(lev).array(mfi);
+            const auto vof = volume_frac ? (*volume_frac)(lev).const_array(mfi)
+                                         : amrex::Array4<amrex::Real const>();
 
-            // For outflow cells (velocity at ghost/boundary cell points outward),
-            // extrapolate by copying interior values to the boundary ghost cell.
+            // For outflow cells (velocity at ghost/boundary cell points
+            // outward), extrapolate by copying interior values to the boundary
+            // ghost cell.
             if (islow && (bx.smallEnd(idim) == domain.smallEnd(idim))) {
                 amrex::ParallelFor(
                     amrex::bdryLo(bx, idim),
                     [=] AMREX_GPU_DEVICE(int i, int j, int k) {
                         const amrex::IntVect iv = {i, j, k};
                         const amrex::IntVect ivm = iv - iv_dir;
-                        if (vel(ivm[0], ivm[1], ivm[2], idim) < 0) {
+                        if (use_vof && vof(ivm) < small_val &&
+                            vof(iv) < small_val) {
                             for (int n = 0; n < ncomp; n++) {
-                                bc_a(ivm[0], ivm[1], ivm[2], n) =
-                                    bc_a(i, j, k, n);
+                                bc_a(ivm, n) = bc_a(iv, n);
                             }
                         }
                     });
@@ -60,10 +66,10 @@ void AdaptInflowBC::operator()(Field& /*field*/, const FieldState /*rho_state*/)
                     [=] AMREX_GPU_DEVICE(int i, int j, int k) {
                         const amrex::IntVect iv = {i, j, k};
                         const amrex::IntVect ivm = iv - iv_dir;
-                        if (vel(i, j, k, idim) > 0) {
+                        if (use_vof && vof(ivm) < small_val &&
+                            vof(iv) < small_val) {
                             for (int n = 0; n < ncomp; n++) {
-                                bc_a(i, j, k, n) =
-                                    bc_a(ivm[0], ivm[1], ivm[2], n);
+                                bc_a(iv, n) = bc_a(ivm, n);
                             }
                         }
                     });
